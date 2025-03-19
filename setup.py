561932259987dbb6302141b307dd9053ce30cf37
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2025-03-19 14:45:18 +0100

    fixes to make installable

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="sparktts",
+    version="0.3.0",
+    author="Ashraff Hathibelagal",
+    description="A powerful, Transformer-based text-to-speech (TTS) tool.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hathibelagal-dev/Spark-TTS",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.11",
+    install_requires=[
+        
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    keywords="ai text-to-speech speech-synthesis nlp transformer voice",
+    project_urls={
+        "Source": "https://github.com/hathibelagal-dev/Spark-TTS",
+        "Tracker": "https://github.com/hathibelagal-dev/Spark-TTS/issues",
+    },
+)
